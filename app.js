2217117ea4931a3625b840b87c37f1e06065a522
Hanbyul-Choi <hanbyul1025@gmail.com>
2023-05-28 15:28:12 +0900

    검색 기능구현 수정

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MjY5MDEwOTNkNDQzM2EyNGFiZTRkMjdiMmM0YTRkMyIsInN1YiI6IjY0NzE5OTI1OTQwOGVjMDExZjJiNDZkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EUGvXNsr4R7VmKWXg8f_-xDsn9NyQOmaoBv-NryWFsc",
   },
 };
-
+// listing movie data
 const listing = () => {
   fetch(
     "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
@@ -23,11 +23,12 @@ const listing = () => {
         let url = `https://image.tmdb.org/t/p/w500/${img}`;
 
         let temp_html = ` <img class="poster" src="${url}" alt="movie">
-                        <h3 style=" text-align: center; text-decoration: underline;">${title}</h3>
+                        <h3 class="movie-title">${title}</h3>
                         <p>${desc}
                         </p>
                         <footer class="rate"><p>Ration : ${rate}</p></footer>
                       `;
+
         // cardList 하위로 card 요소들 붙여넣기
         const $cardList = document.getElementById("card-list");
         const $card = document.createElement("div");
@@ -35,14 +36,14 @@ const listing = () => {
         $card.innerHTML = temp_html;
         $cardList.appendChild($card);
 
-        // click 이벤트 적용
+        // card가 생성 될 때부터 click 이벤트 적용
         $card.addEventListener("click", () => {
           alert(`영화 id : ${id}`);
         });
       });
     })
-    .then((response) => {
-      // 검색기능 구현
+    .then(() => {
+      // 검색 함수 적용
       const $form = document.querySelector(".search-form");
       $form.addEventListener("submit", onclickSearch);
     })
@@ -50,56 +51,26 @@ const listing = () => {
 };
 listing();
 
+// 검색기능 함수
 const onclickSearch = (e) => {
+  // input data 받아오기
   e.preventDefault();
   const $value = document.getElementById("text").value;
-
-  // cardList 하위 요소 삭제 구문
-  const $cardList = document.getElementById("card-list");
   const $card = document.querySelectorAll(".card");
-  $card.forEach((card) => {
-    $cardList.removeChild(card);
-  });
 
-  // 빈 검색창 입력 시
-  if (!$value) listing();
-  else {
-    fetch(
-      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        let sortList = response["results"].filter((i) => {
-          return i["title"].toLowerCase().includes($value);
-        });
+  // 버튼이 눌렸을때 기본적으로 hide class 지우고 진행.
+  $card.forEach((i) => i.classList.remove("hide"));
 
-        sortList.forEach((i) => {
-          let title = i["title"];
-          let rate = i["vote_average"];
-          let desc = i["overview"];
-          let img = i["poster_path"];
-          let id = i["id"];
-          let url = `https://image.tmdb.org/t/p/w500/${img}`;
-
-          let temp_html = ` <img class="poster" src="${url}" alt="movie">
-                        <h3 style=" text-align: center; text-decoration: underline;">${title}</h3>
-                        <p>${desc}
-                        </p>
-                        <footer class="rate"><p>Ration : ${rate}</p></footer>
-                      `;
-          // cardList 하위로 card 요소들 붙여넣기
-          const $cardList = document.getElementById("card-list");
-          const $card = document.createElement("div");
-          $card.classList.add("card");
-          $card.innerHTML = temp_html;
-          $cardList.appendChild($card);
-
-          // click 이벤트 적용
-          $card.addEventListener("click", () => {
-            alert(`영화 id : ${id}`);
-          });
-        });
-      });
+  // input data가 있을때만 hide class 추가
+  if ($value) {
+    const sortList = [...$card].filter((i) => {
+      return !i
+        .querySelector(".movie-title")
+        .textContent.toLowerCase()
+        .includes($value);
+    });
+    sortList.forEach((i) => {
+      i.classList.add("hide");
+    });
   }
 };
