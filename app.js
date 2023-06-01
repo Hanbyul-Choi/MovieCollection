394ef74f8266129522bfad87c18d9ad54cc967f5
Hanbyul-Choi <hanbyul1025@gmail.com>
2023-06-01 20:34:23 +0900

    주석 내용 수정

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const options = {
 
 // 페이지 로드 시 listing movie data
 window.onload = () => {
-  //input focusing
+  //input 태그 focusing
   document.getElementById("input-text").focus();
   fetch(
     "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
@@ -32,12 +32,14 @@ window.onload = () => {
                         <div class="overview">${desc}</div>
                       `;
 
-        // cardList 하위로 card 요소들 붙여넣기
-        const $cardList = document.getElementById("card-list");
+        // $cardList 하위로 넣을 card 만들기
         const $card = document.createElement("div");
         $card.classList.add("card");
         $card.innerHTML = temp_html;
-        $cardList.append($card);
+
+        // cardList 하위로 card HTML 요소들 붙여넣기
+        const $cardList = document.getElementById("card-list");
+        $cardList.appendChild($card);
 
         // card가 생성 될 때부터 click 이벤트 적용
         $card.addEventListener("click", () => {
@@ -51,6 +53,7 @@ window.onload = () => {
 // 검색기능 함수
 const onclickSearch = (e) => {
   e.preventDefault();
+
   // input data 받아오기
   const $inputText = document.getElementById("input-text");
   const $value = $inputText.value;
@@ -60,25 +63,23 @@ const onclickSearch = (e) => {
   if ($value.length === 1) {
     alert("두글자 이상 입력하세요.");
   } else if ($value) {
-    // card title 기준 필터링
+    // card title 포함되는지 필터링
     const sortList = [...$card].filter((i) => {
-      return i // title이 포함되는 node만 필터링
+      return i
         .querySelector(".movie-title")
         .textContent.toLowerCase()
         .includes($value);
     });
-    // 검색어와 일치하는 영화가 없을 때
+    // 검색어와 일치하는 영화가 없을 때 알림
     if (sortList.length === 20) {
       alert("검색어와 일치하는 영화가 없습니다.");
     } else {
+      // 필터링된 node만 보이기
       $card.forEach((i) => i.classList.add("hide"));
-      sortList.forEach((i) => {
-        // 필터링된 node만 보이기
-        i.classList.remove("hide");
-      });
+      sortList.forEach((i) => i.classList.remove("hide"));
     }
   } else {
-    // 아무것도 입력 받지 않았을 때
+    // 아무것도 입력 받지 않았을 때 다시 전부 보여주기
     $card.forEach((i) => i.classList.remove("hide"));
   }
   // 검색 클릭 후 input focusing
