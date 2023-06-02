721634aef44ebcecc63653934444c8c4f8cdd80a
Hanbyul-Choi <hanbyul1025@gmail.com>
2023-06-02 18:57:27 +0900

    수정

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const onclickSearch = (e) => {
         .includes($value);
     });
     // 검색어와 일치하는 영화가 없을 때 알림
-    if (!sortList) {
+    if (!sortList.length) {
       alert("검색어와 일치하는 영화가 없습니다.");
     } else {
       // 필터링된 node만 보이기
